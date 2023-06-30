3bcd37d27159a501471c9a00e605e33e45ee6c6e
D4N1L0200 <danilodantas7002@gmail.com>
2023-06-30 10:58:17 -0300

    Updated engine, sand and water working

--- main.py
+++ main.py
@@ -1,80 +1,80 @@
-from rich import print as rprint
-import cellTypes as cellT
+import time
+
+import pygame
+
+from simulator import Simulation
+
+testing = True
+if testing:
+    width = 10
+    height = 10
+    cell_size = 50
+    padding = 0
+else:
+    width = 100
+    height = 100
+    cell_size = 5
+    padding = 0
+sim = Simulation(width, height)
+
+pygame.init()
+window = pygame.display.set_mode(
+    (
+        width * (cell_size + padding) + padding,
+        height * (cell_size + padding) + padding,
+    )
+)
+clock = pygame.time.Clock()
 
 
-class CellGrid:
-    def __init__(self, width, height, built=True, walled=True):
-        self.width = width
-        self.height = height
-        self.walled = walled
-        self.grid = []
-        # Option to have the grid empty
-        if built:
-            self.build_grid()
-
-    def build_grid(self):
-        # Build the grid, with walls or nothing
-        for y in range(self.height):
-            self.grid.append([])
-            for x in range(self.width):
-                if not self.walled:
-                    self.grid[y].append(cellT.Nothing(x, y))
-                else:
-                    if x == 0 or y == 0 or x == self.width - 1 or y == self.height - 1:
-                        self.grid[y].append(cellT.Wall(x, y))
-                    else:
-                        self.grid[y].append(cellT.Nothing(x, y))
-
-    def get_cell(self, x, y):
-        return self.grid[y][x]
-
-    def push_cell(self, new_cell):
-        self.grid[new_cell.y][new_cell.x] = new_cell
-
-    def push_cells(self, cell_list, new_cell):
-        pass
-        # TODO
-
-    def get_neighbours(self, cell, diagonal=True):
-        neighbours = []
-        if diagonal:
-            # left to right, top to bottom
-            for x in range(cell.x - 1, cell.x + 2):
-                for y in range(cell.y - 1, cell.y + 2):
-                    if x != cell.x or y != cell.y:
-                        neighbours.append(self.get_cell(x, y))
-        else:
-            # left, right, top, bottom
-            for x in range(cell.x - 1, cell.x + 2):
-                if x != cell.x:
-                    neighbours.append(self.get_cell(x, cell.y))
-            for y in range(cell.y - 1, cell.y + 2):
-                if y != cell.y:
-                    neighbours.append(self.get_cell(cell.x, y))
-        return neighbours
+def get_mouse_pos():
+    x, y = pygame.mouse.get_pos()
+    if ((x % (padding + cell_size)) - padding < 0) or (
+        (y % (padding + cell_size)) - padding < 0
+    ):
+        return -1, -1
+    row = y // (padding + cell_size)
+    col = x // (padding + cell_size)
+    return col, row
 
 
-class Simulation:
-    def __init__(self):
-        self.cellGrid = CellGrid(10, 10)
-        self.running = False
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    def tick(self):
-        # Update the grid
-        for row in self.cellGrid.grid:
-            for cell in row:
-                if cell.active:
-                    cell.tick(self.cellGrid)
+    if pygame.mouse.get_pressed()[0]:  # Left Click
+        sim.add_cell(1, get_mouse_pos())
+    if pygame.mouse.get_pressed()[2]:  # Right Click
+        sim.add_cell(2, get_mouse_pos())
 
-    def test(self):
-        # rprint(self.cellGrid.grid)
-        self.cellGrid.push_cell(cellT.Expandable(5, 5))
-        rprint(self.cellGrid.grid)
-        self.tick()
-        rprint(self.cellGrid.grid)
+    window.fill(0)
+    for iy, rowOfCells in enumerate(sim.grid):
+        for ix, cell in enumerate(rowOfCells):
+            if cell.id == 0:
+                color = (16, 16, 16)
+            elif cell.id == 1:
+                color = (255, 255, 255)
+            elif cell.id == 2:
+                color = (0, 0, 255)
+            else:
+                color = (255, 0, 0)
+            pygame.draw.rect(
+                window,
+                color,
+                (
+                    ix * (cell_size + padding) + padding,
+                    iy * (cell_size + padding) + padding,
+                    cell_size,
+                    cell_size,
+                ),
+            )
+    pygame.display.flip()
+    clock.tick(240)
+    if testing:
+        time.sleep(0.2)
+    sim.tick()
 
-
-if __name__ == "__main__":
-    sim = Simulation()
-    sim.test()
-    # sim.run()
+pygame.quit()
+exit()
